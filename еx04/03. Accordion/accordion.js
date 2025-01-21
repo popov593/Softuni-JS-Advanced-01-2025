44e1a22775843e087449566fb7a117adab925ad0
vipe <ivanpopov@Ivans-MacBook-Pro.local>
2025-01-21 08:07:10 +0200

    sync

---
+++ еx04/03. Accordion/accordion.js
@@ -0,0 +1,15 @@
+function toggle() {
+    
+    let button = document.getElementsByClassName("button")[0];
+
+    let text = document.getElementById("extra");
+
+    button.textContent = 
+    button.textContent === 'More' ? "Less" : "More";
+
+    text.style.display =
+    text.style.display === 'none' || text.style.display === '' 
+    ? 'block'
+    : 'none';
+
+}
---
+++ еx04/04. Search in List/search.js
@@ -0,0 +1,33 @@
+function search() {
+   
+   let listItems = document.querySelectorAll('#towns li');
+
+   let input = document.querySelector('input').value;
+
+   let result = document.getElementById('result');
+
+   console.log(listItems);
+
+   let sum = 0;
+
+   for (let li of listItems) {
+
+      if (li.textContent.toLowerCase().includes(input.toLowerCase())) {
+
+         li.style.fontWeight = 'bold';
+         li.style.textDecoration = 'underline';
+
+         sum++;
+
+      } else {
+
+         li.style.fontWeight = '';
+         li.style.textDecoration = '';
+
+      }
+
+   }
+
+   result.textContent = `${sum} matches found`;
+
+}
